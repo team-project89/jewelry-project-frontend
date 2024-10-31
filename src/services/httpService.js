22a907265cf642dd2cce7ca48db926ce74b272d1
sina-tkm <torkmnsina@gmail.com>
2024-10-31 16:11:07 +0330

    feat(auth):feat: implement website authorization

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,7 +1,54 @@
 import axios from "axios";
+import Cookies from "js-cookie";
+
+const getTokenFromCookies = () => {
+  return Cookies.get("access_token");
+};
 
 const BASE_URL = "http://127.0.0.1:8000/";
-const app = axios.create({ baseURL: BASE_URL, withCredentials: true });
+
+const app = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// const refreshToken = async () => {
+//   try {
+//     const response = await app.post("/auth/token/refresh");
+//     Cookies.set("access-token", response.data.accessToken);
+//     return response.data.accessToken;
+//   } catch (error) {
+//     // window.location.href = "/auth";
+//   console.log("error")
+//   }
+// };
+
+// app.interceptors.response.use(
+//   async (response) => response,
+//   async (error) => {
+//     if (error.response && error.response.status === 401) {
+//       const newToken = await refreshToken();
+//       if (newToken) {
+//         error.config.headers["Authorization"] = `Bearer ${newToken}`;
+//         return app(error.config);
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+app.interceptors.request.use(
+  (config) => {
+    const token = getTokenFromCookies();
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 const http = {
   get: app.get,
@@ -10,4 +57,5 @@ const http = {
   put: app.put,
   patch: app.patch,
 };
+
 export default http;
