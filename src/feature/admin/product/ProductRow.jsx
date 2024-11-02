5d6d436d08c66c4a5176ece005c31fe7a973a519
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2024-11-02 22:14:02 +0330

    toPersianNumbers custom hook, tooltip to delete-edit btns in products table added

--- src/feature/admin/product/ProductRow.jsx
+++ src/feature/admin/product/ProductRow.jsx
@@ -1,23 +1,40 @@
-import Table from '@/style/Table'
 import React from 'react'
+import Table from '@/style/Table'
+import { toPersianNumbers, toPersianNumbersWithComma } from '@/utils/toPersianNumbers'
+import { RiDeleteBin5Line } from "react-icons/ri"
+import { FaRegEdit } from "react-icons/fa"
+import { Tooltip } from '@mui/material'
 
-function ProductRow({ product, index}) {
+function ProductRow({ product, index }) {
   return (
     <Table.Row>
-        <td>{ index + 1 }</td>
+        <td>{ toPersianNumbers(index + 1) }</td>
         <td>{ product.name }</td>
         <td>{ product.category}</td>
         <td>{ product?.description || "--" }</td>
         <td>{ product.image1 }</td>
-        <td>{ product.price }</td>
-        <td>{ product.discount_percentage }</td>
-        <td>{ product.price_after_discount }</td>
+        <td>{ toPersianNumbersWithComma(product.price) }</td>
+        <td>{ toPersianNumbers(product.discount_percentage) }</td>
+        <td>{ toPersianNumbersWithComma(product.price_after_discount) }</td>
         <td>{ product.brand }</td>
         <td>{ product.pre_order_available }</td>
-        <td>{ product.pre_order_price }</td>
-        <td>{ product.sales_count }</td>
-        <td>{ product.stock}</td>
-        <td> ### </td>
+        <td>{ toPersianNumbersWithComma(product.pre_order_price) }</td>
+        <td>{ toPersianNumbers(product.sales_count) }</td>
+        <td>{ toPersianNumbers(product.stock)}</td>
+        <td> 
+            <div className="flex items-center gap-x-5">
+                <Tooltip title="حذف" arrow>
+                    <button>
+                        <RiDeleteBin5Line className='w-5 h-5 text-error'/>
+                    </button>
+                </Tooltip>
+                <Tooltip title="ویرایش" arrow>
+                    <button>
+                        <FaRegEdit className='w-5 h-5 text-success'/>
+                    </button>
+                </Tooltip>
+            </div>
+        </td>
     </Table.Row>
   )
 }
